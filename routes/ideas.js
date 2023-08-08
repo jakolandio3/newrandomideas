bece478cb47cc075305f610ede729c3ff7564f98
Jakob Douglas <jakobdouglas1@gmail.com>
2023-08-08 15:42:31 +0930

    got rid of hard coded data and made everything serverside

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,81 +1,70 @@
 const express = require('express');
 const router = express.Router();
-const ideas = [
-	{
-		id: 1,
-		text: 'Positive Newsletter, but the bad kind of positive news really,the type you get from the doctor after a test',
-		tag: 'Technology',
-		username: 'TonyStark',
-		date: '2023-07-08',
-	},
-	{
-		id: 2,
-		text: 'Milk cartons that display how much acid youve taken and how much milk you need to drink to counteract the affects',
-		tag: 'Inventions',
-		username: 'SteveRogers',
-		date: '2023-01-02',
-	},
-	{
-		id: 3,
-		text: 'Location of the closest vending machine in Japan that sells those products Weebs like',
-		tag: 'Software',
-		username: 'BruceBanner',
-		date: '2020-10-18',
-	},
-];
+const Idea = require('../models/Idea');
+
 // get all ideas
-router.get('/', (req, res) => {
-	res.json({ success: true, data: ideas });
+router.get('/', async (req, res) => {
+	try {
+		const ideas = await Idea.find();
+		res.json({ success: true, data: ideas });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, error: 'Something went wrong' });
+	}
 });
 // get specific idea from id
-router.get('/:id', (req, res) => {
-	const idea = ideas.find((idea) => idea.id === +req.params.id);
-	if (!idea) {
-		return res
-			.status(404)
-			.json({ success: false, error: 'Resource not found' });
+router.get('/:id', async (req, res) => {
+	try {
+		const idea = await Idea.findById(req.params.id);
+		res.json({ success: true, data: idea });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, error: 'Something went wrong' });
 	}
-	res.json({ success: true, data: idea });
 });
 // update id
-router.put('/:id', (req, res) => {
-	const idea = ideas.find((idea) => idea.id === +req.params.id);
-	if (!idea) {
-		return res
-			.status(404)
-			.json({ success: false, error: 'Resource not found' });
+router.put('/:id', async (req, res) => {
+	try {
+		const updatedIdea = await Idea.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: {
+					text: req.body.text,
+					tag: req.body.tag,
+				},
+			},
+			{ new: true }
+		);
+		res.json({ success: true, data: updatedIdea });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, error: 'something went wrong' });
 	}
-	idea.text = req.body.text || idea.text;
-	idea.tag = req.body.tag || idea.tag;
-
-	res.json({ success: true, data: idea });
 });
 // remove id
-router.delete('/:id', (req, res) => {
-	const ideaIndex = ideas.findIndex((idea) => idea.id === +req.params.id);
-
-	if (!ideas[ideaIndex]) {
-		return res
-			.status(404)
-			.json({ success: false, error: 'Resource not found' });
+router.delete('/:id', async (req, res) => {
+	try {
+		await Idea.findByIdAndDelete(req.params.id);
+		res.json({ success: true, data: {} });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, error: 'something went wrong' });
 	}
-
-	ideas.splice(ideaIndex, 1);
-
-	res.json({ success: true, data: {} });
 });
 // add idea
-router.post('/', (req, res) => {
-	const idea = {
-		id: ideas.length + 1,
+router.post('/', async (req, res) => {
+	const idea = new Idea({
 		text: req.body.text,
 		tag: req.body.tag,
 		username: req.body.username,
-		date: new Date().toISOString().slice(0, 10),
-	};
-	ideas.push(idea);
-
-	res.json({ success: true, data: idea });
+	});
+	try {
+		const savedIdea = await idea.save();
+		res.json({ success: true, data: savedIdea });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, error: 'Something went wrong' });
+	}
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const port = 5000;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+connectDB();
 const app = express();
 
 //Body parser middleware
@@ -13,4 +16,4 @@ app.get('/', (req, res) => {
 const ideasRouter = require('./routes/ideas');
 app.use('/api/ideas', ideasRouter);
 
-app.listen(port, () => console.log('server listening on port 5000'));
+app.listen(port, () => console.log(`server listening on port ${port}`));
